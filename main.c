023c402aadfaf09664433cdc51058f061f4c9e74
Jan Quest <janquest@gmail.com>
2016-06-14 15:59:32 +0200

    kleine Fehler behoben

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void){
     int wahl = 0, i = 0;
     char buffer[100];
 
-//    system(clear);
+    //system("clear");
     printf("Programm zur Polynomberechnung\n\n");
 
     do{
@@ -26,9 +26,9 @@ int main(void){
             // Auswahl Menuepunkt 1
             case 1:
 
-                printf("Der Grad des Polynoms darf höchstens 5 sein\n");
+                printf("Der Grad des Polynoms darf hoechstens 5 sein\n");
                 printf("Die Form des Polynoms ist p(x) = ax^5 + bx^4 + cx^3 + dx^2 + ex + f\n");
-                printf("Führende Nullen müssen mit angegeben werden p(x) = 2x + 5 = 0x^5 + 0x^4 + 0x^3 + 0x^2 + 2x + 5\n");
+                printf("Fuehrende Nullen muessen mit angegeben werden p(x) = 2x + 5 = 0x^5 + 0x^4 + 0x^3 + 0x^2 + 2x + 5\n");
 
                 printf("\nBitte geben Sie die Koeffitienten nacheinander ein.\n");
                     // Schleife zur Eingabe der Koeffitienten
@@ -56,10 +56,12 @@ int main(void){
                 break;
 
             case 4:
-                printf("Auf Wiedersehn!");
+                printf("Auf Wiedersehn!\n");
+		break;
 
             default:
                 printf("Fehler: Bitte geben Sie eine Zahl zwischen 1 und 4 ein.\n\n");
+		break;
         }
 
 
