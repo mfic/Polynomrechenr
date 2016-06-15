b9ca4396fe44e385f6abcd078d4a323f17552a65
Jan Quest <janquest@gmail.com>
2016-06-15 09:36:09 +0200

    grafik.c angefangen

--- grafik.c
+++ grafik.c
@@ -2,6 +2,24 @@
 
 void grafik(void){
 
+    do
+    {
+        printf("Möchen Sie die Ausgabe auf dem:\n");
+        printf("1. Bildschirm\n");
+        printf("2. .ps-Datei\n");
 
-
+        switch(ausgabe)
+        {
+            case 1:
+                system("gnuplot bildschirm.gnu");
+                break;
+            case 2:
+                system("gnuplot datei.gnu");
+                printf("Die Datei wurde unter grafik.ps gespeichert\n");
+                break;
+            default:
+                printf("Fehler, bitte neue Eingabe.\n");
+                break;
+        }
+    } while (ausgabe != 1 || ausgabe != 2);
 }
