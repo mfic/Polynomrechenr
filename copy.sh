156e94eda5c908427e1755ff50e68221fb532bae
Jan Quest <janquest@gmail.com>
2016-06-27 15:51:30 +0200

    Rechte des Ordners anpassen

--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 # Erstelle Hausaufgabenordner, wenn dieser nicht existiert
 mkdir -p ~/Hausaufgaben/Aufgabe_4
-
+# Rechte des Ordners anpassen
+chmod 755 ~/Hausaufgaben/Aufgabe_4
 # Kopiere Quellcode und make
 rsync -avv *.c Makefile *.gnu ~/Hausaufgaben/Aufgabe_4
