a750f13d8359fe803ca41b01efdc92c4e5f379e0
Jan Quest <janquest@gmail.com>
2016-06-27 12:22:43 +0200

    pfad angepasst

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 # Erstelle Hausaufgabenordner, wenn dieser nicht existiert
-mkdir -p ~/Hausaufgabe
+mkdir -p ~/Hausaufgaben/Aufgabe_4
 
 # Kopiere Quellcode und make
-rsync -avv *.c Makefile *.gnu ~/Hausaufgabe
+rsync -avv *.c Makefile *.gnu ~/Hausaufgaben/Aufgabe_4
